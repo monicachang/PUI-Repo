fc1b3afb23fb6d803133a7fa74155b4a03362e76
Monica Chang <monicac1@andrew.cmu.edu>
2018-10-08 11:55:01 -0400

    finished lab

---
+++ week7_lab/main.js
@@ -0,0 +1,23 @@
+var picts = ["fox1.jpg", "fox2.jpg", "fox3.jpg", "fox4.jpg"]
+var names = ["Nutmeg", "Cinnamon", "Sugar", "Paprika"]
+var ages = [2, 5, 7, 3, 14]
+
+class Animal{
+
+    constructor(img, name, age)
+    {
+        this.pic = img
+        this.name = name
+        this.age = age
+    }
+}
+
+$(document).ready(()=>{
+    console.log("ready")
+
+    var animal = new Animal(picts[2], names[2], ages[2])
+    console.log(animal)
+    $("#animal-img").attr("src", animal.pic);
+    $("#animal-name").html(animal.name);
+    $("#animal-age").html(animal.age + " years old");
+});
