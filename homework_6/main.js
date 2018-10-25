ef6c5019e9c71fd4f4aaa67e3cdb3a30bba07d4d
Monica Chang <monicac1@andrew.cmu.edu>
2018-10-24 20:04:15 -0400

    added session storage variables

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,14 +1,13 @@
-// var picts = ["fanny1.jpg", "fanny2.jpg", "fanny3.jpg", "fannyBlue1.jpg", "fannyBlue2.jpg", 
-//              "fannyBlue3.jpg", "fannyGreen1.jpg", "fannyGreen2.jpg", "fannyGreen3.jpg"]
-// var sources = ["https://mk0theadventuregfnyq.kinstacdn.com/wp-content/uploads/DogHikingGear1.jpg",
-//                "https://mk0theadventuregfnyq.kinstacdn.com/wp-content/uploads/BestDogHikingPacks3.jpg",
-//                "https://www.trekkinn.com/f/13648/136488596_8/ruffwear-approach-pack.jpg",
-//                "https://s3.amazonaws.com/backpackersverse/wp-content/uploads/2016/12/15212859/Interested-In-Hiking-The-Appalachian-Trail-With-A-Dog.jpg",
-//                "https://i2.wp.com/www.hikingwithdogs.xyz/wp-content/uploads/2016/09/IMG_6174.jpg",
-//                "https://i.pinimg.com/originals/87/d1/9a/87d19ae93a331481120c5d97b528c478.jpg",
-//                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjWIIk39hRekEpGR7qhkUPM29yrhaeL7WDdemXLVi0Ot-e31WaWQ",
-//                "https://i.pinimg.com/originals/b0/84/56/b08456b959b1be81d765b0495241902d.jpg",
-//                "https://www.rei.com/media/edb94122-d2d2-4f52-ac52-11e92be1955f?size=784x588"]
+// image sources:
+// "https://mk0theadventuregfnyq.kinstacdn.com/wp-content/uploads/DogHikingGear1.jpg",
+// "https://mk0theadventuregfnyq.kinstacdn.com/wp-content/uploads/BestDogHikingPacks3.jpg",
+// "https://www.trekkinn.com/f/13648/136488596_8/ruffwear-approach-pack.jpg",
+// "https://s3.amazonaws.com/backpackersverse/wp-content/uploads/2016/12/15212859/Interested-In-Hiking-The-Appalachian-Trail-With-A-Dog.jpg",
+// "https://i2.wp.com/www.hikingwithdogs.xyz/wp-content/uploads/2016/09/IMG_6174.jpg",
+// "https://i.pinimg.com/originals/87/d1/9a/87d19ae93a331481120c5d97b528c478.jpg",
+// "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjWIIk39hRekEpGR7qhkUPM29yrhaeL7WDdemXLVi0Ot-e31WaWQ",
+// "https://i.pinimg.com/originals/b0/84/56/b08456b959b1be81d765b0495241902d.jpg",
+// "https://www.rei.com/media/edb94122-d2d2-4f52-ac52-11e92be1955f?size=784x588"
 
 var picts = ["fanny1.jpg", "fannyBlue1.jpg", "fannyGreen1.jpg"]
 var sources = ["https://mk0theadventuregfnyq.kinstacdn.com/wp-content/uploads/DogHikingGear1.jpg",
@@ -56,12 +55,13 @@ $(document).ready(()=>{
     });
 
     // add to cart
-    var itemColor = "";
-    var itemSize = "";
-    var itemImage = "fanny1.jpg";
-    var shipToMe = 0;
-    var pickUpInStore = 0;
-    var itemQuantity = 0;
+    sessionStorage.itemColor = "";
+    sessionStorage.itemSize = "";
+    sessionStorage.itemImage = "fanny1.jpg";
+    sessionStorage.shipToMe = 0;
+    sessionStorage.pickUpInStore = 0;
+    sessionStorage.itemQuantity = 0;
+    sessionStorage.numInCart = 0;
     $("#add-to-cart").click(function() {
         console.log("hello");
 
@@ -69,51 +69,60 @@ $(document).ready(()=>{
         $(".colors").children("span").each(function() {
           if ($(this).hasClass("active-color")) {
             if ($(this).hasClass("color1")) {
-                itemColor = "orange";
-                itemImage = "fanny1.jpg";
+                sessionStorage.itemColor = "orange";
+                sessionStorage.itemImage = "fanny1.jpg";
             }
             else if ($(this).hasClass("color2")) {
-                itemColor = "blue";
-                itemImage = "fannyBlue1.jpg";
+                sessionStorage.itemColor = "blue";
+                sessionStorage.itemImage = "fannyBlue1.jpg";
             }
             else if ($(this).hasClass("color3")) {
-                itemColor = "green";
-                itemImage = "fannyGreen1.jpg";
+                sessionStorage.itemColor = "green";
+                sessionStorage.itemImage = "fannyGreen1.jpg";
             }
           }
         });
-        console.log(itemColor);
+        console.log(sessionStorage.itemColor);
 
         // get item size
         $(".sizes").children("span").each(function() {
           if ($(this).hasClass("active-size")) {
             if ($(this).hasClass("small")) {
-                itemSize = "small";
+                sessionStorage.itemSize = "small";
             }
             else if ($(this).hasClass("medium")) {
-                itemSize = "medium";
+                sessionStorage.itemSize = "medium";
             }
             else if ($(this).hasClass("large")) {
-                itemSize = "large";
+                sessionStorage.itemSize = "large";
             }
           }
         });
-        console.log(itemSize);
+        console.log(sessionStorage.itemSize);
 
         // itemQuantity
-        itemQuantity += parseInt($("#quantity-input").val());
+        sessionStorage.itemQuantity = Number($("#quantity-input").val());
         console.log("quantity: " + $("#quantity-input").val());
+        // add to number in cart
+        console.log(Number(sessionStorage.itemQuantity));
+        
+        // console.log(Number(sessionStorage.numInCart += Number(sessionStorage.itemQuantity)));
+        // sessionStorage.numInCart += Number(sessionStorage.itemQuantity);
+        sessionStorage.numInCart = Number(sessionStorage.numInCart) + Number(sessionStorage.itemQuantity);
+        console.log(Number(sessionStorage.numInCart));
+        // console.log($(".number-in-cart-dot"));
+        $(".number-in-cart-dot").text(sessionStorage.numInCart);
 
         // delivery
         if ($(".ship-to-me").children([0]).prop("checked")) {
-            shipToMe += 1;
+            sessionStorage.shipToMe += Number(sessionStorage.itemQuantity);
         }
-        else if ($(".pick-up").children([0]).prop("checked")) {
-            pickUpInStore += 1;
+        // else if ($(".pick-up").children([0]).prop("checked")) {
+        else {
+            sessionStorage.pickUpInStore += Number(sessionStorage.itemQuantity);
         }
-        console.log(shipToMe);
-        console.log(pickUpInStore);
 
+        
     });
 
 
