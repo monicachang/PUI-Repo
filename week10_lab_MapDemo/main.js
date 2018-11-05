336dc9670542f192b4e6806e11d7755528afb1c7
Monica Chang <monicac1@andrew.cmu.edu>
2018-11-05 11:31:56 -0500

    did google maps api demo for week10 lab

---
+++ week10_lab_MapDemo/main.js
@@ -0,0 +1,29 @@
+/*
+ * main.js
+ * -----------------------------------------------
+ * Small javascript demo of the Google Map API
+ */
+
+
+
+//WRITE NEW CODE HERE 
+//uncomment the code below for var myHome
+//update the latitude and longitude for where you want the center of your map to be 
+var myHome = {lat: 40.442784, lng: -79.943131}
+
+function myMap() {
+	//Defines the properties of the map 
+	var mapProp= {
+		center: myHome, //defines where the map is centered on load
+		zoom:5, //specifies map initial zoom level 
+	};
+
+	//WRITE NEW CODE HERE
+	//uncomment code below
+	//first select the div we created for the google map on index.html 
+	//then create a new google map passing in two arguments:
+	//1) where you want the map to render (the div) and 2) the map properties variable
+	var elem = document.getElementById("googleMap");
+	var map = new google.maps.Map(elem, mapProp);
+}
+
