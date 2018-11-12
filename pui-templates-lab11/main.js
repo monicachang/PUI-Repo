d42ef78e86aa015d68d9dd6620d4528c3a1f781a
Monica Chang <monicac1@andrew.cmu.edu>
2018-11-12 11:17:00 -0500

    intiated pui lab11

---
+++ pui-templates-lab11/main.js
@@ -0,0 +1,21 @@
+// TODO (Step 2): fill in code creating products objects here
+var products = {
+    "cinnamonBuns": [    ]
+}
+
+$(document).ready(function(){
+    // TODO (Step 4): fill in code for changing selection-template here
+
+
+    // TODO (Step 7): add function call for 1st update to detail-template
+
+
+    // TODO: (Step 8): add code below for changing product details on click
+
+
+});
+
+// TODO (Step 6): fill in function for updating detail-template based on id
+function updateProductDetail(id) {
+
+}
