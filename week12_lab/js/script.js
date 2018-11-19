218d63589f6fe40ece21281724b0865bdd47fe62
Monica Chang <monicac1@andrew.cmu.edu>
2018-11-19 10:52:58 -0500

    initiated week 12 lab

---
+++ week12_lab/js/script.js
@@ -0,0 +1,36 @@
+//When .toggle() btn is clicked the div should appear and disappear
+$("#toggleFunc").on("click", function() {
+	$(".outer").toggle();
+});
+
+//When smooth .toggle() is clicked, the div should toggle slow
+$("#toggleSmooth").on("click", function() {
+	//ADD CODE HERE
+});
+
+//When .slideUp() is clicked, the div should swipe up to disappear slowly
+$("#slidingUp").on("click", function() {
+	//ADD CODE HERE
+});
+
+//When .slideDown() btn is clicked, the div should swipe down to appear slowly
+$("#slidingDown").on("click", function() {
+	//ADD CODE HERE
+});
+
+/* You can customize your own animation with .animate() */
+
+//When .animate() slide up to height btn is clicked, the div should swipe up to change height to 20px
+$("#slidingUpHeight").on("click", function() {
+	//ADD CODE HERE
+});
+
+//When .animate() slide down to height btn is clicked, the div should swipe down to change height to 300px
+$("#slidingDownHeight").on("click", function() {
+    //ADD CODE HERE
+});
+
+//When the .animate() slide up and down to height btn is clicked, the div should swipe up to change height to 20px and then back down to change height to 50px
+$("#slidingUpDownHeight").on("click", function() {
+    //ADD CODE HERE
+});
