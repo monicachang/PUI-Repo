7834c1a6ea8a3129b476ba2aca7683e011c14ebf
Monica Chang <monicac1@andrew.cmu.edu>
2018-11-19 11:36:20 -0500

    finished script 1 and 2 of d3 portion of week12 lab

--- week12_lab/lab12-part2-dataviz-master/js/script1.js
+++ week12_lab/lab12-part2-dataviz-master/js/script1.js
@@ -2,10 +2,42 @@
 
 var myChart = d3.select("#myChart");
 
-var svg = myChart.append("svg").attr('width', 50).attr("height", 50);
+var svg = myChart.append("svg").attr('width', 105).attr("height", 105);
 
-svg.append("circle")
-    .attr("cx", 25)
-    .attr("cy", 25)
-    .attr("r", 25)
-    .attr("fill", "red");
+//Group
+d3.select("#myChart")
+  .append("svg")
+  .attr("width", 200).attr("height", 200)
+  .append("g")
+  .attr("transform", "translate(0, 0)")
+  .append("circle")
+  .attr("cx", 25).attr("cy", 25)
+  .attr("r", 25)
+  .attr("fill", "red")
+ 
+d3.select("g")
+  .append("circle")
+  .attr("cx", 80).attr("cy", 80)
+ .attr("r", 25)
+ .attr("fill", "yellow");
+
+
+d3.selectAll("circle")
+    .on("mouseover", mouseover);
+
+function mouseover(d) {
+     d3.select(this)
+         .transition()
+         .attr("transform", "translate(20, 20)");
+
+    tooltip.text(d3.select(this).attr("fill"))
+     .style("visibility", "visible")
+     .style("top", event.pageY+"px")
+     .style("left", event.pageX+"px");
+}
+
+var tooltip = d3.select("#myChart")
+             .append("div")
+             .style("position", "absolute")
+             .style("z-index", "10")
+             .style("visibility", "hidden");
--- week12_lab/lab12-part2-dataviz-master/js/script2.js
+++ week12_lab/lab12-part2-dataviz-master/js/script2.js
@@ -9,3 +9,43 @@ var MHCIday = [
   { item: 'misc', hours: 1}
 ];
 
+var pie = d3.pie()
+  .value(function(d) { return d.hours });
+
+var slices = pie(MHCIday);
+// console.log(slices);
+
+var arc = d3.arc()
+  .innerRadius(0)
+  .outerRadius(50);
+
+// helper that returns a color based on an ID
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var svg = d3.select("#myChart").append("svg");
+
+var g = svg.append("g").attr("transform", "translate(50, 50)");
+
+g.selectAll("path")
+    .data(slices)
+    .enter()
+    .append("path")
+    .attr("d", arc)
+    .attr("fill", function(s) {return color(s.data.item);})
+    // on mouse over
+    .on("mousemove", mousemove);
+
+var tooltip = d3.select("#myChart")
+                .append("div")
+                .style("visibility", "hidden")
+                .style("z-index", "10")
+                .style("position", "absolute");
+
+function mousemove(d){
+    console.log(this);
+    console.log(d);
+    tooltip.style("visibility", "visible")
+            .text(d.data.item +": "+d.data.hours)
+            .style("top", event.pageY+"px")
+            .style("left", event.pageX+"px");
+}
