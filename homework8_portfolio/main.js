fc397c181b04a1d0f111de4dbd564e00c126a702
Monica Chang <monicac1@andrew.cmu.edu>
2018-11-23 14:21:36 -0500

    added moving sidebar

--- homework8_portfolio/main.js
+++ homework8_portfolio/main.js
@@ -4,3 +4,33 @@
 //     e.preventDefault();
 //     $("#wrapper").toggleClass("toggled");
 // });
+$(document).ready(function(){
+
+    $("#work-nav").click(function(){
+        // $('#sidebar-wrapper').removeClass('col-4').addClass('col-3');
+
+        $('#sidebar-wrapper').toggleClass('col-4 col-2');
+        $('#content-wrapper').toggleClass('col-8 col-10');
+
+        if ($("#sidebar-brand").children[0].css("text-align") == "right") {
+            $("#sidebar-brand").children[0].css("text-align") = "left";
+        }
+        else {
+            $("#sidebar-brand").children[0].css("text-align") = "right";
+        }
+
+
+        // var moveWidth = $("#sidebar-wrapper").width();
+        // $("#sidebar-wrapper").animate({
+        //     width: "-="+moveWidth/3,
+        //     opacity: "0.25" 
+        // });
+
+        // $("#content-wrapper").animate({
+        //     left: "-="+moveWidth/3,
+        //     width: "+="+moveWidth/3,
+        // });
+
+    });
+
+});
