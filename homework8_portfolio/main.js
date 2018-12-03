92eabfa1c8acc0f57f028cf7510d89f4496ecddf
Monica Chang <monicac1@andrew.cmu.edu>
2018-12-03 05:32:13 -0500

    did some home page animation

--- homework8_portfolio/main.js
+++ homework8_portfolio/main.js
@@ -6,6 +6,28 @@
 // });
 $(document).ready(function(){
 
+    // animate home page on scroll
+    $(window).scroll(function() {
+        if ($("body").scrollTop() > 100 || $("#welcome-statement1").scrollTop() > 100) {
+            $('#welcome-statement1').removeClass("fadeInRight");        
+            $('#welcome-statement1').addClass("fadeOut");
+        } else {
+            $('#welcome-statement1').removeClass("fadeOut");        
+            $('#welcome-statement1').addClass("fadeIn");
+        }
+
+        if ($('#welcome-statement1').hasClass("fadeOut")) {
+            $('#welcome-statement2').css("visibility", "visible"); 
+            $('#welcome-statement2').attr("class", "animated fadeIn slow");
+            // also fade in explore button
+            $('#explore-button').css("visibility", "visible"); 
+            $('#explore-button').attr("class", "animated fadeIn slow");
+        } else {
+            $('#welcome-statement2').attr("class", "animated fadeOut slow");
+            // $('#welcome-statement2').css("visibility", "hidden"); 
+        }
+    });
+
     // if the category was expanded or active, slide it down
     $(".active-category").next().slideDown();
     $(".open-dropdown").slideDown();
@@ -17,8 +39,8 @@ $(document).ready(function(){
         $('#sidebar-wrapper').toggleClass('col-md-4 col-md-3');
         $('#content-wrapper').toggleClass('col-md-8 col-md-9');
 
-        $('#objective-statement').removeClass("fadeInRight");        
-        $('#objective-statement').toggleClass("fadeOut");
+        // $('#objective-statement').removeClass("fadeInRight");        
+        // $('#objective-statement').toggleClass("fadeOut");
 
     });
 
